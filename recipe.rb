5fa608bdd84eeac45a059bc262018fab78691df8
anhvu15 <antonvu09@gmail.com>
2015-04-23 21:49:35 -0700

    finish lesson

--- recipe.rb
+++ recipe.rb
@@ -1,1 +1,12 @@
-puts "Hello"
+Recipe = Struct.new(:ingredients,:method)
+recipe = Recipe.new({avocados: 4,jalapenos: 2},["Peel / Slice avocados","Chop jalapenos into small dice"])
+puts "ingredients"
+recipe.ingredients.each do |key,value|
+	puts "#{key}: #{value}"
+end
+puts "\nMethods"
+
+recipe.method.each_with_index do |value,index|
+	puts "step #{index +1}: #{value}"
+end
+
--- student.rb
+++ student.rb
@@ -1,16 +1,29 @@
-class Student
-	attr_reader :first_name,:last_name,:grade
-	def initialize(first_name,last_name,grade)
-		@first_name = first_name
-		@last_name = last_name
-		@grade = grade
-	end
+# class Student
+# 	attr_reader :first_name,:last_name,:grade
+# 	def initialize(first_name,last_name,grade)
+# 		@first_name = first_name
+# 		@last_name = last_name
+# 		@grade = grade
+# 	end
 
+# 	def senior?
+# 		grade == 12
+# 	end
+
+# 	def to_s
+# 		"#{last_name}, #{first_name}"
+# 	end
+# end
+
+Student = Struct.new(:first_name,:last_name,:grade) do 
 	def senior?
-		grade == 12
+		grade == 12		
 	end
-
 	def to_s
 		"#{last_name}, #{first_name}"
 	end
 end
+
+student = Student.new("Anh","Vu")
+puts student
+puts student.grade
